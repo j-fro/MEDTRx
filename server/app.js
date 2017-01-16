625934d510c9bbf217c45eea2ddfbbb9e6b5b733
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-16 10:11:39 -0600

    additional server setup

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 // Modules
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-let app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+
+// bodyParser setup
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function() {
     console.log('Listening on port', app.get('port'));
 });
+
+// Serve static files
+app.use(express.static('public'));
+
+module.exports = app;
