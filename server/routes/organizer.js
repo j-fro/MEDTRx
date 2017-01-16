496e7055064692997309c9c2c0ae2091d937b9db
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-16 10:41:43 -0600

    spiked out organizer routes

--- server/routes/organizer.js
+++ server/routes/organizer.js
@@ -5,4 +5,14 @@ router.get('/', function(req, res) {
     res.send('Here waiting for the organizer..');
 });
 
+router.post('/:deviceId', function(req, res) {
+    console.log('Received post from device:', req.params.deviceId, 'with data', req.body);
+    res.sendStatus(501);
+});
+
+router.put('/', function(req, res) {
+    console.log('Received put with:', req.body);
+    res.sendStatus(501);
+});
+
 module.exports = router;
