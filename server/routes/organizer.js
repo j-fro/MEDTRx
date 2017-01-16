21b8ae0432614f1f8f4b271030aa54d058303a2e
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-16 15:37:47 -0600

    added documentation

--- server/routes/organizer.js
+++ server/routes/organizer.js
@@ -8,16 +8,22 @@ router.get('/', function(req, res) {
     res.send('Here waiting for the organizer..');
 });
 
+/*
+ * API endpoint for the device to make new entries. Only one entry per device
+ * per day should be created. On a POST request, make a new status entry in the
+ * DB if one of these conditions are true:
+ * 1) there have been no entries for the sending device (determined by the devices key in
+ *    the request URL)
+ * 2) the most recent entry for the sending device was not on the current day
+ */
 router.post('/:deviceId', function(req, res) {
     console.log('Received post from device:', req.params.deviceId, 'with data', req.body);
     findMostRecentStatusByDeviceId(req.params.deviceId, function(err, existing) {
         if (err) {
             console.log(err);
+        // If there are no entries for this device or the most recent entry was
+        // not today, make a new entry
         } else if (!existing || existing.created.getDate() !== new Date().getDate()) {
-            console.log('existing', existing);
-            console.log('existing date', existing.created.getDate());
-            console.log('current date', new Date().getDate());
-            console.log('Creating a new entry');
             createStatus(req.params.deviceId, req.body.status, function(err) {
                 if(err) {
                     console.log(err);
@@ -37,6 +43,13 @@ router.put('/', function(req, res) {
     res.sendStatus(501);
 });
 
+/*
+ * Takes a device ID and a callback function and queries the database. If there
+ * were no errors, executes the callback with the most recent entry for that
+ * device in the form (null, [entry]) where null indicates that there was no
+ * error. The [entry] CAN be undefined if the device ID was not found in the DB.
+ * Otherwise, executes the callback with the error
+ */
 function findMostRecentStatusByDeviceId(deviceId, callback) {
     pg.connect(connString)
         .then(function(client, done) {
