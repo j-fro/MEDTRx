eadf613c5b0b207807c130fff97a77d21700eaf4
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-17 11:39:46 -0600

    cleaned up and commented dashboard functions

--- public/scripts/controllers/dashboardController.js
+++ public/scripts/controllers/dashboardController.js
@@ -1,35 +1,30 @@
 angular.module('msApp').controller('DashboardController', ['$scope', '$http', function($scope, $http) {
     console.log('ng');
     var today = new Date();
-    today.setDate(today.getDate() - today.getDay());
-    $scope.currentWeekStart = today;
+    var endOfWeek = today;
+    endOfWeek.setDate(today.getDate() - today.getDay());
+    $scope.currentWeekStart = endOfWeek;
     $scope.currentWeekEnd = new Date(today).setDate(today.getDate() + 7);
-    // var weekDates = [0, 1, 2, 3, 4, 5, 6].map(function(day) {
-    //     return new Date(new Date(today).setDate(today.getDate() + day));
-    // });
-    // console.log(weekDates);
 
-
+    // Holds the statuses for the week indexed 0-6
     $scope.statuses = {};
 
     $scope.getStatuses = function() {
         $http.get('/organizer/bob@bobmail.com')
             .then(function(result) {
-                console.log(result);
                 result.data.forEach(function(status) {
-                    console.log(new Date(status.created));
                     var day = new Date(status.created).getDay();
-                    console.log('index:', day);
                     $scope.statuses[day] = status.status;
                 });
             });
     };
 
+    // Set all days before today to false
     for(i = 0; i < new Date().getDay(); i++) {
         if(!$scope.statuses[i]) {
             $scope.statuses[i] = false;
         }
     }
-
+    // Then get the real statuses
     $scope.getStatuses();
 }]);
