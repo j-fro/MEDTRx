3a2cf56bf532bf4b2635cc4e9c8747e8a0a2b2b9
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-17 11:32:13 -0600

    fixed status display by day

--- public/scripts/controllers/dashboardController.js
+++ public/scripts/controllers/dashboardController.js
@@ -4,17 +4,11 @@ angular.module('msApp').controller('DashboardController', ['$scope', '$http', fu
     today.setDate(today.getDate() - today.getDay());
     $scope.currentWeekStart = today;
     $scope.currentWeekEnd = new Date(today).setDate(today.getDate() + 7);
-    var weekDates = [0, 1, 2, 3, 4, 5, 6].map(function(day) {
-        return new Date(new Date(today).setDate(today.getDate() + day));
-    });
-    console.log(weekDates);
-    $scope.sunday = "Complete";
-    $scope.monday = "Complete";
-    $scope.tuesday = "--";
-    $scope.wednesday = "--";
-    $scope.thursday = "--";
-    $scope.friday = "--";
-    $scope.saturday = "--";
+    // var weekDates = [0, 1, 2, 3, 4, 5, 6].map(function(day) {
+    //     return new Date(new Date(today).setDate(today.getDate() + day));
+    // });
+    // console.log(weekDates);
+
 
     $scope.statuses = {};
 
@@ -24,12 +18,18 @@ angular.module('msApp').controller('DashboardController', ['$scope', '$http', fu
                 console.log(result);
                 result.data.forEach(function(status) {
                     console.log(new Date(status.created));
-                    var index = weekDates.indexOf(new Date(status.created));
-                    console.log('index:', index);
-                    $scope.statuses[index] = status.status;
+                    var day = new Date(status.created).getDay();
+                    console.log('index:', day);
+                    $scope.statuses[day] = status.status;
                 });
             });
     };
+
+    for(i = 0; i < new Date().getDay(); i++) {
+        if(!$scope.statuses[i]) {
+            $scope.statuses[i] = false;
+        }
+    }
 
     $scope.getStatuses();
 }]);
