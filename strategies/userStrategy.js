e692349aac1e32fe7749caad39fcf8fdcff197ca
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-17 14:22:26 -0600

    added password compare logic

--- strategies/userStrategy.js
+++ strategies/userStrategy.js
@@ -8,6 +8,40 @@ passport.use('local', new LocalStrategy({
     passReqToCallback: true
 }, function(req, username, password, done) {
     console.log('hit local strategy callback');
+    pg.connect(connString)
+    .then(function(client) {
+        client.query('SELECT * FROM users WHERE email=$1 LIMIT 1', [username])
+        .then(function(result) {
+            user = result.rows[0];
+            if(user) {
+                console.log('User:', user);
+                compare(password, user.password, function(err, isMatch) {
+                    if (isMatch) {
+                        console.log('Successful login');
+                        done(null, user, {
+                            message: 'Login Successful'
+                        });
+                    } else {
+                        console.log('bad pw');
+                        done(null, false, {
+                            message: 'Incorrect credentials'
+                        });
+                    }
+                });
+            } else {
+                console.log('no user');
+                done(null, false, {
+                    message: 'Incorrect credentials'
+                });
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
 }));
 
 module.exports = passport;
