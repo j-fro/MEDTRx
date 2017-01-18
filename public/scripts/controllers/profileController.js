e98bf9e545522c1e8412ef2afbc6d3c7c9c1f84c
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-18 14:04:27 -0600

    locked profile view behind auth

--- public/scripts/controllers/profileController.js
+++ public/scripts/controllers/profileController.js
@@ -1,3 +1,16 @@
-angular.module('msApp').controller('ProfileController', ['$scope', function($scope) {
+angular.module('msApp').controller('ProfileController', ['$scope', '$window', 'AuthFactory', function($scope, $window, AuthFactory) {
     console.log('profile');
+
+    AuthFactory.isLoggedIn()
+        .then(function(result) {
+            if (result.status === 200) {
+                $scope.loggedIn = true;
+            } else {
+                $window.location.href = '#!/login';
+            }
+        })
+        .catch(function(err) {
+            console.log(err);
+            $window.location.href = '#!/login';
+        });
 }]);
