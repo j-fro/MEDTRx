c63fac4ab44379a2a46cd00b32ff40708f8d1a43
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-19 20:18:08 -0600

    query last status for a user to confirm if they've checked in today

--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 var pg = require('pg');
 
 var connString = process.env.DATABASE_URL || 'postgres://localhost:5432/medSched';
@@ -12,4 +14,27 @@ function connect(callback) {
     });
 }
 
-module.exports = {connect: connect, connString: connString};
+function findUsersLastStatus(userId, callback) {
+    connect(function(client, end) {
+        var query = `
+        SELECT created FROM statuses
+        JOIN devices ON statuses.device_name=devices.device_id
+        WHERE devices.user_id=$1
+        ORDER BY created DESC LIMIT 1
+        `;
+        client.query(query, [userId], function(err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Result', result.rows);
+                callback(result.rows[0]);
+            }
+        });
+    });
+}
+
+module.exports = {
+    connect: connect,
+    connString: connString,
+    findUsersLastStatus: findUsersLastStatus
+};
