f7ca174a5418e710c8d5566b0b4de9cffdd1dd29
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-28 13:13:34 -0600

    converted register to knex

--- server/routes/register.js
+++ server/routes/register.js
@@ -4,31 +4,50 @@ var path = require('path');
 var pg = require('pg');
 var encrypt = require('../../utils/auth').encrypt;
 var db = require('../../utils/database/db');
+const knex = require('../../utils/database');
 var router = express.Router();
 
 router.post('/', function(req, res) {
     console.log('Hit post route:', req.body);
-    encrypt(req.body.password, function(err, hash) {
-        if(err) {
+    encrypt(req.body.password, (err, hash) => {
+        if (err) {
             console.log(err);
             res.sendStatus(500);
         } else {
-            db.users.insert.byUsernameAndHash(req.body.email, hash, function(err, result) {
-                if(err) {
-                    console.log(err);
-                    res.sendStatus(500);
-                } else {
+            knex
+                .insert({email: req.body.email, password: hash})
+                .into('users')
+                .then(() => {
                     createUserDevice(req.body.email, req.body.deviceId)
-                        .then((message) => {
-                            console.log(message);
-                            res.sendStatus(201);
-                        })
-                        .catch((err) => {
+                        .then(() => res.sendStatus(201))
+                        .catch(err => {
                             console.log(err);
                             res.sendStatus(500);
                         });
-                }
-            });
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.sendStatus(500);
+                });
+            // db.users.insert.byUsernameAndHash(req.body.email, hash, function(
+            //     err,
+            //     result
+            // ) {
+            //     if (err) {
+            //         console.log(err);
+            //         res.sendStatus(500);
+            //     } else {
+            //         createUserDevice(req.body.email, req.body.deviceId)
+            //             .then(message => {
+            //                 console.log(message);
+            //                 res.sendStatus(201);
+            //             })
+            //             .catch(err => {
+            //                 console.log(err);
+            //                 res.sendStatus(500);
+            //             });
+            //     }
+            // });
         }
     });
 });
@@ -40,13 +59,14 @@ function createUserDevice(userEmail, deviceId) {
         if (deviceId) {
             db.users.select.byUserEmail(userEmail, function(user) {
                 if (user) {
-                    db.devices.insert.one(deviceId, user.id)
-                        .then(() => resolve("Success"))
+                    db.devices.insert
+                        .one(deviceId, user.id)
+                        .then(() => resolve('Success'))
                         .catch(err => reject(err));
                 }
             });
         } else {
-            resolve("No device Id present");
+            resolve('No device Id present');
         }
     });
 }
