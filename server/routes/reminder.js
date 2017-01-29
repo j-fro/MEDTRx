744392f55e552132240c0b81e2f56c79eb3bf507
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-28 18:06:09 -0600

    converted reminder router to knex

--- server/routes/reminder.js
+++ server/routes/reminder.js
@@ -3,50 +3,80 @@ const pg = require('pg');
 const schedule = require('node-schedule');
 const db = require('../../utils/database/db');
 const auth = require('../../utils/auth');
+const knex = require('../../utils/database');
 let router = express.Router();
 
 router.use(auth.checkIfAuthenticated);
 
 router.get('/', (req, res) => {
-    db.reminders.select.oneByUserId(req.user.id, (err, result) => {
-        if (err) {
+    knex
+        .select()
+        .from('reminders')
+        .where('user_id', req.user.id)
+        .limit(1)
+        .then(reminders => res.send(reminders[0]))
+        .catch(err => {
             console.log(err);
             res.sendStatus(501);
-        } else {
-            res.send(result);
-        }
-    });
+        });
 });
 
 router.put('/', function(req, res) {
     console.log('Updating reminder for', req.user);
     var timeIn = new Date(req.body.reminderTime);
-    var reminderTime = timeIn.getHours() + ':' + timeIn.getMinutes() + ':' + timeIn.getSeconds();
+    var reminderTime = timeIn.getHours() +
+        ':' +
+        timeIn.getMinutes() +
+        ':' +
+        timeIn.getSeconds();
 
-    db.reminders.select.oneByUserId(req.user.id, function(err, result) {
-        if (err) {
+    knex
+        .select()
+        .from('reminders')
+        .where('user_id', req.user.id)
+        .limit(1)
+        .then(reminders => {
+            if (reminders[0]) {
+                updateReminder(reminders[0], reminderTime)
+                    .then(() => res.sendStatus(200))
+                    .catch(err => {
+                        console.log(err);
+                        res.sendStatus(501);
+                    });
+            } else {
+                insertReminder(req.user.id, reminderTime)
+                    .then(() => res.sendStatus(200))
+                    .catch(err => {
+                        console.log(err);
+                        res.sendStatus(501);
+                    });
+            }
+        })
+        .catch(err => {
             console.log(err);
-        } else if (result) {
-            console.log(result);
-            db.reminders.update.timeByUserId(result.id, reminderTime, function(err) {
-                if (err) {
-                    console.log(err);
-                    res.sendStatus(500);
-                } else {
-                    res.sendStatus(200);
-                }
-            });
-        } else {
-            db.reminders.insert.byUserId(req.user.id, reminderTime, function(err) {
-                if (err) {
-                    console.log(err);
-                    res.sendStatus(500);
-                } else {
-                    res.sendStatus(200);
-                }
-            });
-        }
-    });
+            res.sendStatus(501);
+        });
 });
 
 module.exports = router;
+
+function updateReminder(reminder, reminderTime) {
+    return new Promise((resolve, reject) => {
+        knex
+            .from('reminders')
+            .where('id', reminder.id)
+            .update('reminder_time', reminderTime)
+            .then(() => resolve())
+            .catch(err => reject(err));
+    });
+}
+
+function insertReminder(userId, reminderTime) {
+    return new Promise((resolve, reject) => {
+        knex
+            .insert({user_id: userId, reminder_time: reminderTime})
+            .into('reminders')
+            .then(() => resolve())
+            .catch(err => reject(err));
+    });
+}
