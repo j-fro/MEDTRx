479e3c74bb6ac50653734b90946df9c31e60e18e
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-28 18:35:19 -0600

    converted scheduling to knex (i hope)

--- utils/schedule.js
+++ utils/schedule.js
@@ -1,22 +1,22 @@
 const schedule = require('node-schedule');
 const sendEmail = require('../utils/sendGridClient').sendEmail;
-const db = require('../utils/database/db');
+const knex = require('./database');
 const twilio = require('./twilioClient');
 const DAY_IN_MS = 86400000;
 
 function scheduleReminder(userId) {
-    db.reminders.select.oneByUserId(userId, function(err, reminder) {
-        if (reminder) {
-            let reminderDate = buildReminderDate(reminder.reminder_time);
-            console.log('Scheduling for', reminder.user_id, 'at', reminderDate);
-            addToSchedule(userId, reminderDate);
-        }
+    knex.select().from('reminders').where('user_id', userId).then(reminders => {
+        let reminder = reminders[0];
+        let reminderDate = buildReminderDate(reminders.reminder_time);
+        console.log('Scheduling for', reminder.user_id, 'at', reminderDate);
+        addToSchedule(userId, reminderDate);
     });
 }
 
 function scheduleAllReminders() {
-    db.reminders.select
-        .all()
+    knex
+        .select()
+        .from('reminders')
         .then(reminders => {
             reminders.forEach(reminder => {
                 console.log(
@@ -29,67 +29,53 @@ function scheduleAllReminders() {
                 addToSchedule(reminder.user_id, reminderDate);
             });
         })
-        .catch(err => {
-            console.log(err);
-        });
+        .catch(err => console.log(err));
 }
 
 function addToSchedule(userId, reminderDate) {
     console.log('In addToSchedule');
     schedule.scheduleJob(reminderDate, function() {
         console.log('In addToSchedule: scheduling a job at:', reminderDate);
-        db.statuses.select.mostRecentByUserId(userId, function(status) {
-            console.log('In addToSchedule: found a status:', status);
-            if (status) {
-                status.created = new Date(status.created);
-                console.log(
-                    'In addToSchedule: status time is now:',
-                    status.created.getHours() +
-                        ':' +
-                        status.created.getMinutes()
-                );
-            }
-            if (
-                !status ||
-                    new Date().getTime() - status.created.getTime() >= DAY_IN_MS
-            ) {
-                console.log('In addToSchedule: sending a reminder for', userId);
-                db.contacts.select
-                    .allByUserIdAndType(userId, 'phone')
-                    .then(contacts => {
-                        console.log('Contacts:', contacts);
-                        contacts.forEach(function(contact) {
-                            twilio.sendSms(
-                                contact.contact,
-                                'You missed your check-in today'
-                            );
-                        });
-                    })
-                    .catch(err => console.log(err));
-                db.contacts.select
-                    .allByUserIdAndType(userId, 'email')
-                    .then(contacts => {
-                        console.log(
-                            'In addToSchedule: email contacts:',
-                            contacts
-                        );
-                        let message = {
-                            type: 'text/plain',
-                            value: 'You missed your medication check-in today'
-                        };
-                        contacts.forEach(contact => {
-                            sendEmail(
-                                {email: contact.contact},
-                                'You missed your check-in',
-                                message
-                            ).catch(err => console.log(err.response.body.errors));
-                        });
-                    });
-            }
-        });
+        knex
+            .select()
+            .from('statuses')
+            .where('user_id', userId)
+            .orderBy('created', 'desc')
+            .limit(1)
+            .then(statuses => {
+                let status = statuses[0];
+                console.log('In addToSchedule: found a status:', status);
+                if (status) {
+                    status.created = new Date(status.created);
+                    console.log(
+                        'In addToSchedule: status time is now:',
+                        status.created.getHours() +
+                            ':' +
+                            status.created.getMinutes()
+                    );
+                }
+                if (
+                    !status ||
+                        new Date().getTime() - status.created.getTime() >=
+                            DAY_IN_MS
+                ) {
+                    console.log(
+                        'In addToSchedule: sending a reminder for',
+                        userId
+                    );
+                    sendSmsReminder(userId).catch(err => console.log(err));
+                    sendEmailReminder(userId).catch(err => console.log(err));
+                }
+            })
+            .catch(err => console.log(err));
         scheduleReminder(userId);
     });
 }
+
+module.exports = {
+    scheduleReminder: scheduleReminder,
+    scheduleAllReminders: scheduleAllReminders
+};
 
 function buildReminderDate(reminderTime) {
     let today = new Date();
@@ -105,7 +91,49 @@ function buildReminderDate(reminderTime) {
     return reminderDate;
 }
 
-module.exports = {
-    scheduleReminder: scheduleReminder,
-    scheduleAllReminders: scheduleAllReminders
-};
+function sendSmsReminder(userId) {
+    return new Promise((resolve, reject) => {
+        knex
+            .select()
+            .from('contacts')
+            .where('user_id', userId)
+            .andWhere('contact_type', 'phone')
+            .then(contacts => {
+                console.log('Contacts:', contacts);
+                contacts.forEach(function(contact) {
+                    twilio.sendSms(
+                        contact.contact,
+                        'You missed your check-in today'
+                    );
+                });
+                resolve();
+            })
+            .catch(err => reject(err));
+    });
+}
+
+function sendEmailReminder(userId) {
+    return new Promise((resolve, reject) => {
+        knex
+            .select()
+            .from('contacts')
+            .where('user_id', userId)
+            .andWhere('contact_type', 'email')
+            .then(contacts => {
+                console.log('In addToSchedule: email contacts:', contacts);
+                let message = {
+                    type: 'text/plain',
+                    value: 'You missed your medication check-in today'
+                };
+                contacts.forEach(contact => {
+                    sendEmail(
+                        {email: contact.contact},
+                        'You missed your check-in',
+                        message
+                    ).catch(err => console.log(err.response.body.errors));
+                });
+                resolve();
+            })
+            .catch(err => reject(err));
+    });
+}
